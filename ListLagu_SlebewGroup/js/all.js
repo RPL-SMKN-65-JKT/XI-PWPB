83c68ea3a51c01a3557935adc4c46856d4a96ad5
fbari687 <109676917+fbari687@users.noreply.github.com>
2022-09-10 13:28:28 +0700

    SlebewGroup_ListLagu
    Anggota Kelompok:
    1. Fian Kurniawan
    2. Fathul Bari
    3. Muhammad Revikhasha Farabi Putera
    4. Pratama Ridho Priyanto
    5. Zikryandri Riedwan

---
+++ ListLagu_SlebewGroup/js/all.js
@@ -0,0 +1,32 @@
+let allMusic = [
+    {
+      name: "Mantra Hujan",
+      artist: "Kobo Kanaeru (Ayang Gweh ❤)",
+      img: "ayang",
+      src: "ayang"
+    },
+    {
+        name: "Last Friday Night",
+        artist: "Katy Perry",
+        img: "katy",
+        src: "katy"
+    },
+    {
+        name: "West",
+        artist: "Warren Hue",
+        img: "west",
+        src: "west"
+    },
+    {
+        name: "New Tooth",
+        artist: "Rich Brian",
+        img: "new",
+        src: "new"
+    },
+    {
+        name: "7!! Orange",
+        artist: "SevenOops",
+        img: "cicak",
+        src: "cicak"
+    },
+  ];
