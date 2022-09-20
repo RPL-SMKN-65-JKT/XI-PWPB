0b7ad3c97f702bf3aed474e47f297f324ac5e850
Arif <109660195+ArifIbrahim@users.noreply.github.com>
2022-09-20 20:41:43 +0700

    Oyasumi

---
+++ Muhammad_Arif_Ibrahim_21_Navigasi/waktu.js
@@ -0,0 +1,18 @@
+window.onload = function() { jam(); }
+   
+    function jam() {
+     var e = document.getElementById('jam'),
+     d = new Date(), h, m, s;
+     h = d.getHours();
+     m = set(d.getMinutes());
+     s = set(d.getSeconds());
+   
+     e.innerHTML = h +':'+ m +':'+ s;
+   
+     setTimeout('jam()', 1000);
+    }
+   
+    function set(e) {
+     e = e < 10 ? '0'+ e : e;
+     return e;
+    }
