d870d866448dc77eed24aead5ba84c52bf3efed7
AlanPratama <109650474+AlanPratama@users.noreply.github.com>
2022-09-23 21:40:18 +0700

    Delete ALAN PRATAMA RUSFI (03)/ALAN PRTAMA_03_PORTOFOLIO directory

--- ALAN PRATAMA RUSFI (03)/ALAN PRTAMA_03_PORTOFOLIO/script.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.querySelector('.menu-toggle input');
-const nav = document.querySelector('nav ul');
-
-menuToggle.addEventListener('click', function(){
-    nav.classList.toggle('slide');
-});
