8aba083a2f30e42643e531f40d3c0a5f83e622d2
Aqiilah733 <109678680+Aqiilah733@users.noreply.github.com>
2022-10-17 10:36:05 +0700

    Aqiilah_07_Portofolio

---
+++ Aqiilah_07_Portofolio/script.js
@@ -0,0 +1,26 @@
+/* efek ngetik */
+const txtElement = ['A', 'Q', 'I', 'I', 'L', 'A', 'H'];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = '';
+let words = '';
+
+(function ngetik(){
+
+if(count == txtElement.length){
+    count = 0;
+}
+
+currentTxt = txtElement[count];
+
+words = currentTxt.slice(0, ++txtIndex);
+document.querySelector('.efek-ngetik').textContent = words;
+
+if(words.length == currentTxt.length){
+    count++;
+    txtIndex = 0;
+}
+
+setTimeout(ngetik, 600);
+
+})();
