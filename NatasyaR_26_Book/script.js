e8f7811c08ff368fbe804c0aa4914ee003243c02
NatasyaR <109677543+NatasyaR@users.noreply.github.com>
2022-11-14 09:39:40 +0700

    Delete NatasyaR_26_Book directory

--- NatasyaR_26_Book/script.js
+++
@@ -1,8 +0,0 @@
-// delay possible clicks until page is turned
-document.getElementById('book').addEventListener('click', function(e) {
-  e = this
-  e.style.pointerEvents = "none"
-  setTimeout(function(){
-    e.style.pointerEvents = ""
-  }, 2000)
-})
