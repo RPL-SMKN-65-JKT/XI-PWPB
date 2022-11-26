cd2f0fd746d26f774ed1b3bc0a655670f6e5d480
Aqiilah733 <109678680+Aqiilah733@users.noreply.github.com>
2022-11-26 07:23:13 +0700

    Aqiilah_07_Album

---
+++ Aqiilah_07_Album/script.js
@@ -0,0 +1,7 @@
+document.getElementById('book').addEventListener('click', function(e) {
+    e = this
+    e.style.pointerEvents = "none"
+    setTimeout(function(){
+      e.style.pointerEvents = ""
+    }, 2000)
+  })
