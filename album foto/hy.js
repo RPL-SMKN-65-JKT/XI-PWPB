46a7291606758cd64b695cb91a5f6d40f2d8b2b9
Krtka2711 <109679103+Krtka2711@users.noreply.github.com>
2022-11-27 00:12:32 +0700

    kartika aprillia zahra (18)_album foto

---
+++ album foto/hy.js
@@ -0,0 +1,9 @@
+// delay possible clicks until page is turned
+document.getElementById('book').addEventListener('click', function(e) {
+    e = this
+    e.style.pointerEvents = "none"
+    setTimeout(function(){
+      e.style.pointerEvents = ""
+    }, 2000)
+  })
+  
