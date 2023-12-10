b704ff15b619a3f47c4bf07078d106f7f4013569
AlanPratama <109650474+AlanPratama@users.noreply.github.com>
2023-12-11 00:46:10 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+    "./node_modules/flowbite/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')({
+      charts: true,
+  }),
+  ],
+}
+
